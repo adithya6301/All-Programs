34349051bc12bf282ac8b6f41fe64aa5485c5b13
adithya6301 <adithyabnaireroor.9d@gmail.com>
2025-11-05 00:27:16 +0530

    Linked List Q1

---
+++ Lab 4/Q1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node* next;
+}Node;
+
+Node *head=NULL;
+
+Node* createNode(int data)
+{
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if(newNode==NULL)
+    {
+        printf("\nMemory allocation failed!");
+        exit(1);
+    }
+    newNode->data=data;
+    newNode->next=NULL;
+    return;
+}
