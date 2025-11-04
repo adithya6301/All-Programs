a19b4c3aa7078d3a49ac0d133a970c82e3d0a4db
adithya6301 <adithyabnaireroor.9d@gmail.com>
2025-11-04 20:20:44 +0530

    Linear Search Final update

--- LAB 1/Linear_search.c
+++ LAB 1/Linear_search.c
@@ -1,5 +1,5 @@
 /*1. Given an array of n integers and a key element, write a C program to search the element
-using linear search*/
+using linear search     */
 
 #include<stdio.h>
 #include<stdlib.h>
